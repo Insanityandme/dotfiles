fe4719d6709eb2e3ff74c4b1009ea9e4cffb083d
Insanityandme <bengtegardbook@gmail.com>
2014-11-12 17:53:18 +0100

    changed README.md

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,5 +1,9 @@
 # Enable colorized output, note that this is a mac OS X-only solution. Install coreutil if used on other platforms.
 alias ls='ls -Gp'
 
-# Show hidden files, proper list format, sorted after dates, colorized output, slash to indicate dirs, symbols to indicate different types of files.
+# Show hidden files, proper list format, sorted after dates, colorized output, 
+# slash to indicate dirs, symbols to indicate different types of files.
 alias ll='ls -alrtGpF'
+
+# Going down one directory
+alias ..='cd .."
